d4e7880da208036634e43ece3fda710e152daa60
SusanBird <susanbird391@gmail.com>
2022-05-16 14:58:30 -0700

    useEffect to get data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    async function load() {
+      const rawResponse = await fetch('https://pokedex-alchemy.herokuapp.com/');
+      const data = await rawResponse.json(); 
+      
+      setPokemons(data);
+    }
+
+    load();
+  });
+
+
+
   return (
     <div className="App">
 
