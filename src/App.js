396837dda8f81a4cbd528d7ba469a6a7947ecbc1
SusanBird <susanbird391@gmail.com>
2022-05-16 15:22:58 -0700

    map over destructured data

--- src/App.js
+++ src/App.js
@@ -9,16 +9,24 @@ function App() {
       const rawResponse = await fetch('https://pokedex-alchemy.herokuapp.com/');
       const data = await rawResponse.json(); 
       
+      console.log(data);
       setPokemons(data);
     }
 
     load();
-  });
-
-
+  }, []);
 
   return (
     <div className="App">
+      <header className='App-header'>
+        {pokemons.map(({ pokemon, attack, defense, url_image }, i) => <div key={pokemon.pokemon + i}>
+          <p>name: {pokemon}</p>
+          <p>attack: {attack}</p>
+          <p>defense: {defense}</p>
+          <img src={url_image} />
+        </div>
+        )}
+      </header>
 
     </div>
   );
