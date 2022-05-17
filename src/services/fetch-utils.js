dac809e1b34f4b537fdb121c836077ceeb7e971f
SusanBird <susanbird391@gmail.com>
2022-05-16 18:03:50 -0700

    updated pokemon.js

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,5 +1,5 @@
 export async function getPokemons(searchFilter) {
-  const rawResponse = await fetch(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${searchFilter}`);
+  const rawResponse = await fetch(`/.netlify/functions/movies?searchFilter=${searchFilter}`);
   const data = await rawResponse.json(); 
 
   return data;
